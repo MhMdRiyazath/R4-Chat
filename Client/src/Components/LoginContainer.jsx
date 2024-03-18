fb18b65bbf23a4b49dab7a6ae95cb6fcdc9ce4e9
MhMdRiyazath <mohamedriyazath.prof@gmail.com>
2024-03-18 12:58:12 +0530

    update login signup page

--- Client/src/Components/LoginContainer.jsx
+++ Client/src/Components/LoginContainer.jsx
@@ -1,31 +1,58 @@
 import { Button, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 const login = () => {
- 
+
+  const [isSignUp, setSignUp] = useState(true)
+
   return (
     <div className='bg-[#f4f5f8] h-[90vh] w-[90vw] flex rounded-2xl'>
       <div className='flex flex-[0.3] justify-center items-center bg-purple-100 rounded-l-2xl'>
         <img src='../communication.png' alt='Logo' width={150} />
       </div>
       <div className='flex flex-[0.7] flex-col gap-5 justify-center items-center'>
-        <p className='text-purple-500 font-bold text-lg'>Login To Your Account</p>
-        <TextField
-          className='w-[50%] max-w-[300px]'
-          id="outlined-basic"
-          label="Username"
-          variant="outlined" />
-        <TextField
-          className='w-[50%] max-w-[300px]'
-          id="outlined-basic"
-          label="Password"
-          type='password'
-          variant="outlined"
-          autoComplete='current-password'
-        />
-        <Button variant="contained">Login</Button>
 
+        {isSignUp ?
+          <>
+            <p className='text-purple-500 font-bold text-lg'>Create a new Account </p>
+            <TextField
+              className='w-[50%] max-w-[300px]'
+              label="Username"
+              variant="outlined" />
+            <TextField
+              className='w-[50%] max-w-[300px]'
+              label="Email Address"
+              variant="outlined" />
+            <TextField
+              className='w-[50%] max-w-[300px]'
+              label="Password"
+              type='password'
+              variant="outlined"
+              autoComplete='current-password'
+            />
+            <Button variant="contained">SignUp</Button>
+          </>
+          :
+          <>
+            <p className='text-purple-500 font-bold text-lg'>Login To Your Account</p>
+            <TextField
+              className='w-[50%] max-w-[300px]'
+              label="Username"
+              variant="outlined" />
+            <TextField
+              className='w-[50%] max-w-[300px]'
+              label="Password"
+              type='password'
+              variant="outlined"
+              autoComplete='current-password'
+            />
+            <Button variant="contained">Login</Button>
+          </>
+        }
+
+        <p>{isSignUp ? "Already have an account?":"Don't have an account?"} <span className='text-purple-500 cursor-pointer' onClick={() => setSignUp(!isSignUp)}>{isSignUp ? "Login":"SignUp"}</span></p>
       </div>
+
     </div>
   )
 }
--- Server/index.js
+++ Server/index.js
@@ -1,32 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const app = express();//initialize express server
+const app = express(); //initialize express server
 
-app.get('/', (req, res) => {
-    res.send('Api is running...');
-})
+app.get("/", (req, res) => {
+  res.send("Api is running...");
+});
 
-dotenv.config();//initialize dotenv 
+dotenv.config(); //initialize dotenv
 
 //connect to the database
-const connectDB = async()=>{
-    try{
-    const connect =await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected');
-}catch(err){
+const connectDB = async () => {
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
     console.log(err);
-}
-}
+  }
+};
 
-connectDB();//call the connectDB function
+connectDB(); //call the connectDB function
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
-
-
